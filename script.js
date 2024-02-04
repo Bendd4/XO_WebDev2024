20a8f45f68aeb93dc84b3b441a37786e192533e9
Waradon N <31185649+WaradonN@users.noreply.github.com>
2024-02-04 23:51:02 +0700

    final

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function leaveTheMatchEarly() {
         return false;
     }
 }
+
+function openPlayerDetail() {
+    document.getElementById("playerDetailExpanded").style.top = "0px";
+}
+function closePlayerDetail() {
+    document.getElementById("playerDetailExpanded").style.top = "-450px";
+}
