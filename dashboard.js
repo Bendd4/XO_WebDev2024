50b63ddd5d95c15a5e8e71f829e18d76e73fed13
Waradon N <31185649+WaradonN@users.noreply.github.com>
2024-02-20 09:52:54 +0700

    cant logout in small display fixma

--- dashboard.js
+++ dashboard.js
@@ -5,6 +5,13 @@ btnLogout.addEventListener("click", function () {
     window.location.href = "index.html";
 })
 
+const btnLogoutSmall = document.querySelector("#btn-logoutSmall");
+btnLogoutSmall.addEventListener("click", function () {
+    firebase.auth().signOut();
+    console.log("logout completed");
+    window.location.href = "index.html";
+})
+
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         console.log("User :", user);
